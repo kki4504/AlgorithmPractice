a6cd5b0ca682564361bc42b6d5a6eb489030cac8
kki4504 <rhkddlf9876@gmail.com>
2022-08-30 15:35:43 +0900

    [BOJ] 1110번 더하기 사이클

---
+++ 1110.js
@@ -0,0 +1,16 @@
+const input = parseInt(require('fs').readFileSync('./dev/stdin').toString().trim());
+
+let X = input;
+let first = 0;
+let second = 0;
+let count = 0;
+
+while (count < 10) {
+  count += 1
+  if(count < 2) {
+    
+  }
+
+  if(input === X)
+    break;
+}
