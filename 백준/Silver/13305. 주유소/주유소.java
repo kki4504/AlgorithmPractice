9b960776d290777a354875feb3d2de9ba13a9181
Kwang il Kim <57711744+kki4504@users.noreply.github.com>
2023-09-19 22:52:21 +0900

    [Silver III] Title: 주유소, Time: 256 ms, Memory: 32164 KB -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+interface Main {
+    static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] lengthArray = new int[N - 1];
+        int[] oilPriceArray = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N - 1; i++) {
+            lengthArray[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            oilPriceArray[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int minPrice = 100000000;
+        int result = 0;
+        for (int i = 0; i < lengthArray.length; i++) {
+            if (oilPriceArray[i] < minPrice) {
+                minPrice = oilPriceArray[i];
+            }
+
+            result += minPrice * lengthArray[i];
+        }
+
+        System.out.println(result);
+    }
+}
